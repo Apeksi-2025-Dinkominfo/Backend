3b170739d6d6b898a110ef6cecca9c7e80cc1a90
Bima Bimo <satriaabimanyu595@gmail.com>
2024-10-07 16:27:58 +0700

    feat: go back to Prisma NodeJs Backend, because Boss doesnt want to use Strapi, Register form for participant now can CRUD

---
+++ controllers/newsController.js
@@ -0,0 +1,9 @@
+export const getNews = (req, res) => {};
+
+export const getNewsById = (req, res) => {};
+
+export const createNews = (req, res) => {};
+
+export const updateNews = (req, res) => {};
+
+export const deleteNews = (req, res) => {};
